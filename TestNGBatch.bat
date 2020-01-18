43048ebcf69220f29e1dd77a8688b882a0c4b205
Binita2020 <60012729+Binita2020@users.noreply.github.com>
2020-01-19 02:03:33 +0530

    Add files via upload

--- TestNGBatch.bat
+++ TestNGBatch.bat
@@ -1,14 +1,7 @@
-<?xml version="1.0" encoding="UTF-8"?>
-
-<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd">
-
--<suite name="Suite">
-
-<!-- Suite -->
-
-<test name="Test" thread-count="5"> </test>
-
-<!-- Test -->
-
-
-</suite>
+cd C:\Users\BinitaRawat\Desktop\binita\Workspace\ProjectJavaGit
+set ProjectPath=C:\Users\BinitaRawat\Desktop\binita\Workspace\ProjectJavaGit
+echo %ProjectPath%
+set classpath=%ProjectPath%\bin;%ProjectPath%\lib\*
+echo %classpath%
+java org.testng.TestNG %ProjectPath%\testng.xml
+pause
