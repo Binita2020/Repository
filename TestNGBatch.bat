0d4d9f98a2bf0ef7f625120a54a322fa5e3affa1
Binita2020 <60012729+Binita2020@users.noreply.github.com>
2020-01-19 01:18:50 +0530

    Add files via upload

--- TestNGBatch.bat
+++ TestNGBatch.bat
@@ -1,11 +1,14 @@
-<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd" >
-<suite name="Selenium Tests" verbose="3" parallel="tests"
-    thread-count="1">
- 
- 
-    <test name="Test1">
-        <classes>
-            <class name="TestGetClass" />
-        </classes>
-    </test>
+<?xml version="1.0" encoding="UTF-8"?>
+
+<!DOCTYPE suite SYSTEM "https://testng.org/testng-1.0.dtd">
+
+-<suite name="Suite">
+
+<!-- Suite -->
+
+<test name="Test" thread-count="5"> </test>
+
+<!-- Test -->
+
+
 </suite>
